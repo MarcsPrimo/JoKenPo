12d714ddc74535801bebcbf91f5a571106a2bca5
MarcsPrimo <48611016+MarcsPrimo@users.noreply.github.com>
2019-03-30 21:02:23 -0300

    Add files via upload

---
+++ JogoPedraPapelTesoura/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace JogoPedraPapelTesoura
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Saida2 = new System.Windows.Forms.Label();
+            this.Saida1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(384, 200);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ESPERANDO";
+            // 
+            // Saida2
+            // 
+            this.Saida2.AutoSize = true;
+            this.Saida2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Saida2.Location = new System.Drawing.Point(133, 49);
+            this.Saida2.Name = "Saida2";
+            this.Saida2.Size = new System.Drawing.Size(22, 25);
+            this.Saida2.TabIndex = 6;
+            this.Saida2.Text = "0";
+            // 
+            // Saida1
+            // 
+            this.Saida1.AutoSize = true;
+            this.Saida1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Saida1.Location = new System.Drawing.Point(843, 502);
+            this.Saida1.Name = "Saida1";
+            this.Saida1.Size = new System.Drawing.Size(22, 25);
+            this.Saida1.TabIndex = 7;
+            this.Saida1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "PONTOS:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(741, 502);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 25);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "PONTOS:";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.SeaShell;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(8, 522);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(216, 63);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "REINICIAR JOGO";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(343, 228);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 180);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(343, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 180);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.IndianRed;
+            this.button3.BackgroundImage = global::JogoPedraPapelTesoura.Properties.Resources.dado1;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Location = new System.Drawing.Point(524, 453);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 129);
+            this.button3.TabIndex = 1;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SpringGreen;
+            this.button2.BackgroundImage = global::JogoPedraPapelTesoura.Properties.Resources.dado3;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(230, 453);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 129);
+            this.button2.TabIndex = 0;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrange;
+            this.button1.BackgroundImage = global::JogoPedraPapelTesoura.Properties.Resources.dado2;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(377, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 129);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(916, 594);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Saida1);
+            this.Controls.Add(this.Saida2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Saida2;
+        private System.Windows.Forms.Label Saida1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ JogoPedraPapelTesoura/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoPedraPapelTesoura
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //COMANDO QUE DESATIVA O RESIZE MANUAL.
+            this.FormBorderStyle = FormBorderStyle.FixedDialog;
+
+
+        }
+        //DECLARANDO VARIAVEIS GLOBAIS.
+        Random rnd = new Random();
+        int valor1 = 0;
+        int valorCpu1 = 0;
+        
+        //MÉTODO QUE SETA AS REGRAS DO JOGO E DEFINE O FIM APÓS ACUMULADO 5 PONTOS.
+        public void Teste()
+        {
+
+           
+            if (valor1 == 1 && valorCpu1 == 1)
+            {
+                label1.Text = "**EMPATE**";
+            }
+            if (valor1 == 2 && valorCpu1 == 2)
+            {
+                label1.Text = "**EMPATE**";
+            }
+            if (valor1 == 3 && valorCpu1 == 3)
+            {
+                label1.Text = "**EMPATE**";
+            }
+            if (valor1 == 1 && valorCpu1 == 2)
+            {
+                label1.Text = "**GANHOU**";
+                Saida1.Text = (int.Parse(Saida1.Text) + 1).ToString();
+            }
+            if (valor1 == 1 && valorCpu1 == 3)
+            {
+                label1.Text = "**PERDEU**";
+                Saida2.Text = (int.Parse(Saida2.Text) + 1).ToString();
+            }
+            if (valor1 == 3 && valorCpu1 == 2)
+            {
+                label1.Text = "**PERDEU**";
+                Saida2.Text = (int.Parse(Saida2.Text) + 1).ToString();
+            }
+            if (valor1 == 3 && valorCpu1 == 1)
+            {
+                label1.Text = "**GANHOU**";
+                Saida1.Text = (int.Parse(Saida1.Text) + 1).ToString();
+            }
+            if (valor1 == 2 && valorCpu1 == 1)
+            {
+                label1.Text = "**PERDEU**";
+                Saida2.Text = (int.Parse(Saida2.Text) + 1).ToString();
+            }
+            if (valor1 == 2 && valorCpu1 == 3)
+            {
+                label1.Text = "**GANHOU**";
+                Saida1.Text = (int.Parse(Saida1.Text) + 1).ToString();
+            }
+
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button1.Enabled = true;
+
+
+            if (Saida1.Text == "5" | Saida2.Text == "5")
+            {
+                this.BackColor = Color.Green;
+                Saida1.Text = "0";
+                Saida2.Text = "0";
+
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button1.Enabled = false;
+
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //GERA A JOGADA ALEÁTORIA DO ADVERSÁRIO.
+            int x1 = rnd.Next(1, 4);
+
+            pictureBox1.BackgroundImage = Image.FromFile("dado" + x1 + ".png");
+
+            //GERA A JOGADO QUE O JOGADOR ESCOLHEU.
+            pictureBox2.BackgroundImage = Image.FromFile("dado1.png");
+
+            button2.Enabled = false;
+           
+            //SETA O VALOR DAS VARIAVEIS GLOBAIS.
+            valor1 = 1;
+            valorCpu1 = x1;
+
+            //CHAMA O MÉTODO.
+            Teste();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x2 = rnd.Next(1, 4);
+
+            pictureBox1.BackgroundImage = Image.FromFile("dado" + x2 + ".png");
+
+            pictureBox2.BackgroundImage = Image.FromFile("dado2.png");
+
+            button3.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+            valor1 = 2;
+            valorCpu1 = x2;
+
+            Teste();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x3 = rnd.Next(1, 4);
+
+            pictureBox1.BackgroundImage = Image.FromFile("dado" + x3 + ".png");
+
+            pictureBox2.BackgroundImage = Image.FromFile("dado3.png");
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button1.Enabled = false;
+
+            valor1 = 3;
+            valorCpu1 = x3;
+
+            Teste();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //REINICIA DO JOGO.
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button1.Enabled = true;
+            label1.Text = "ESPERANDO";
+
+            this.BackColor = Color.SkyBlue;
+        }
+    }
+}
